fbb3496991bd6dacb46aa8f452792cb906e8875f
omri zaher <omriza5@gmail.com>
2021-11-12 22:24:39 +0200

    change db url connection

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 // const connectionURL = "mongodb://127.0.0.1:27017";
 const connectionURL =
-  "mongodb+srv://omriza5:amosh100@cluster0.dql37.mongodb.net/test";
+  "mongodb+srv://omriza5:amosh100@cluster0.dql37.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const databaseName = "omriBank";
 
 module.exports = () => {
