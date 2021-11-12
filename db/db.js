a481a288379a043ec884a6acec555899d8774dab
omri zaher <omriza5@gmail.com>
2021-11-12 21:30:31 +0200

    modify: switched to mongodb atlas

--- db/db.js
+++ db/db.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 
-const connectionURL = "mongodb://127.0.0.1:27017";
+// const connectionURL = "mongodb://127.0.0.1:27017";
+const connectionURL =
+  "mongodb+srv://omriza5:amosh100@cluster0.dql37.mongodb.net/test";
 const databaseName = "omriBank";
 
 module.exports = () => {
-  mongoose.connect(`${connectionURL}/${databaseName}`, {}, (err, client) => {
+  mongoose.connect(`${connectionURL}`, {}, (err, client) => {
     if (err) return console.log("Connection Failed...", err);
 
     console.log("Connected to MongoDB...");
   });
 };
+
+// /${databaseName}
