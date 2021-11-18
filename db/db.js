016266f829cf0c37325c8d6481bd3491d2043fb0
omri zaher <omriza5@gmail.com>
2021-11-18 15:49:20 +0200

    modify: db url to environment var

--- db/db.js
+++ db/db.js
@@ -3,17 +3,18 @@ require("dotenv").config();
 
 const url =
   "mongodb+srv://omriza5:amosh100@cluster0.dql37.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-// module.exports = () => {
-//   mongoose.connect(`${process.env.DATABASE_URL}`, {}, (err, client) => {
-//     if (err) return console.log("Connection Failed...", err);
 
-//     console.log("Connected to MongoDB Atlas...");
-//   });
-// };
 module.exports = () => {
-  mongoose.connect(url, {}, (err, client) => {
+  mongoose.connect(`${process.env.DATABASE_URL}`, {}, (err, client) => {
     if (err) return console.log("Connection Failed...", err);
 
     console.log("Connected to MongoDB Atlas...");
   });
 };
+// module.exports = () => {
+//   mongoose.connect(url, {}, (err, client) => {
+//     if (err) return console.log("Connection Failed...", err);
+
+//     console.log("Connected to MongoDB Atlas...");
+//   });
+// };
